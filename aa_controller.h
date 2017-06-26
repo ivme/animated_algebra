5877453b97dc938c0c67aa771a506fe388018869
Chad Mills <chad.mills@gmail.com>
2017-06-25 23:47:29 -0400

    fix typo

--- aa_controller.h
+++ aa_controller.h
@@ -13,7 +13,7 @@
 class aa_controller {
 public:
 
-	// propmts user for (a,b,c) of ax^2 +bx + c
+	// prompt user for (a,b,c) of ax^2 +bx + c
 	std::array<int,3> prompt_for_quadratic_coeffs(bool allow_negatives);
 	void animate_quadratic_factorization(std::array<int,3> coeffs);
 	void animate_quadratic_factorization(int a, int b, int c);
