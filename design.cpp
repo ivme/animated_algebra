51eb70d4f91954cd8fedf14acc7b156fd3166fcd
Chad Mills <chad.mills@gmail.com>
2017-07-04 00:20:27 -0400

    use type erasure for node interface.  store wrapped object by value.

--- design.cpp
+++ design.cpp
@@ -2,38 +2,60 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <memory>
+#include <utility>
 
 /*
-Store a pointer to the appropriate rendering function in each node.
-Use ordinary inheritance to provide an interface to nodes.
+Still store a pointer to the rendering function in each node,
+but use type erasure to provide node interface.
 Pros:
-	Each node can render itself.
-	
-	The user of the library can extend both the set of nodes
-	and the set of rendering functions without editing the 
-	underlying code of the library.
+	No downcasts are needed in the rendering functions.
+
+	renderable_concepts of different underlying type can
+	be stored in the same container (e.g. vector), which
+	makes it easy to build a tree out of them -- each one
+	could hold a vector of pointers to its children.
+
+	Set of nodes and renderers is still extensible while
+	maintaining encapsulation of the library.  User-defined
+	node types and renderers no longer need to inherit
+	from library types.
 
 Cons:
-    To maintain a consistent interface, the render_func
-    must take a node& as an parameter, which means that each
-    render_func must downcast its parameter to the particular
-    node type it renders.
+	In order to access the wrapped renderable object t
+	from a pointer to renderable_concept, the user would
+	need to downcast to the correct renderable_model subclass.
 
-    The user must explicitly assign the appropriate render_func
-    to each node as it is created.  With this solution, it would
-    be tedious to make a factory type that builds nodes and
-    assigns the appropriate render_func.  The user would also
-    have to work to extend this factor type as she adds new
-    node types and rendering functions.
-
-    It would be tedious to group rendering functions
-    into rendering systems.
+	Even after downcasting, the user would still need to access
+	the wrapped type through the member variable t, which is
+	awkward, e.g.
+		struct circle {int radius;};
+		renderable_model<circle> circle_model{{5}}; // set radius = 5
+		renderable_concept *concept_ptr = &circle_model;
+		auto recovered_circle = static_cast<*renderable_model<circle>>(concept_ptr);
+		recovered_circle->t.radius; // awkward extra t
 */
 
-struct node {
-	std::function<void(node&)> render_func;
-	void render() {render_func(*this);}
+
+struct renderable_concept {
+  virtual void render() const = 0;
 };
+
+template<class T>
+struct renderable_model : renderable_concept {
+  T t;
+  template<class...Us>
+  renderable_model(Us&&...us ):
+    t{std::forward<Us>(us)...}
+  {}
+  std::function<void(const T&)> render_func;
+  void set_render_func(std::function<void(const T &)> fn) {render_func = fn;}
+  void render() const final override {
+	render_func(t);
+  }
+};
+
+struct node {};
 
 struct rect_node : public node {
 	int width;
@@ -41,16 +63,15 @@ struct rect_node : public node {
 };
 
 struct text_node : public node {
+	text_node(std::string text_) : text(text_) {}
 	std::string text;
 };
 
-void render_text_node_normal(node &n) {
-	auto tn = static_cast<text_node &>(n); // don't like this cast
+void render_text_node_default(const text_node &tn) {
 	std::cout << tn.text << std::endl;
 }
 
-void render_text_node_caps(node &n) {
-	auto tn = static_cast<text_node &>(n); // don't like this cast
+void render_text_node_caps(const text_node &tn) {
 	std::string upper_text = tn.text;
 	for (auto &c : upper_text) {c = std::toupper(c);}
 	std::cout << upper_text << std::endl;
@@ -58,10 +79,9 @@ void render_text_node_caps(node &n) {
 
 
 int main() {
-	text_node tn;
-	tn.text = "hello";
-	tn.render_func = render_text_node_normal;
-	tn.render();
-	tn.render_func = render_text_node_caps;
-	tn.render();
+	auto rm_tn = renderable_model<text_node>{"hello"};
+	rm_tn.set_render_func(render_text_node_default);
+	rm_tn.render();
+	rm_tn.set_render_func(render_text_node_caps);
+	rm_tn.render();
 }
