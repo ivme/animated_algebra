3fb0ef4a49d9f74a28b17252e142d62a6c81fceb
Chad Mills <chad.mills@gmail.com>
2017-07-05 10:44:33 -0400

    replace make_node factory func with templated constructor

--- design.cpp
+++ design.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <memory>
 #include <utility>
+#include "liven/graphics.h"
 
 /*
 Use type erasure for node interface, but store the wrapped
@@ -12,32 +13,25 @@ Pros:
 	If we store a pointer to the wrapped object prior to
 	node constructed, we still have access to the wrapped
 	object in a reasonably natural way (see tn in main()).
-
 	Given a generic_node, we can examine the address of
 	the wrapped object via ptr_to_wrapped -- even if we
 	don't know the type of the wrapped object.
-
 	Easy to make a templated factory function that selects the
 	correct rendering function via overload resolution.
-
 	Also easy to group rendering functions into rendering
 	classes, and to leverage library rendering functionality
 	in a custom renderer (see caps_renderer) for all but a few 
 	special cases (see template specialization of
 	caps_renderer::render<text_node>).
-
 	No downcasts are needed in the rendering functions.
-
 	generic_nodes of different wrapped types can
 	be stored in the same container (e.g. vector), which
 	makes it easy to build a tree out of them -- each one
 	could hold a vector of pointers to its children.
-
 	Set of nodes and renderers is still extensible while
 	maintaining encapsulation of the library.  User-defined
 	node types and renderers no longer need to inherit
 	from library types.
-
 Cons:
 	It's necessary to store a pointer to the wrapped object
 	before node construction in order to be able to access
@@ -49,6 +43,7 @@ Cons:
 
 #ifndef DEFAULT_RENDERER
 #define DEFAULT_RENDERER normal_renderer
+class normal_renderer;
 #endif
 
 struct renderable_concept {
@@ -58,35 +53,45 @@ struct renderable_concept {
 
 template<class T>
 struct renderable_model : renderable_concept {
-  std::shared_ptr<T> t;
-  std::function<void(const T&)> render_func;
-  template<class...Us>
-  renderable_model(std::function<void(const T &)> render_func_, std::shared_ptr<T> t_):
-  	render_func{render_func_},
-    t{t_}
-  {}
-  std::shared_ptr<const void> ptr_to_wrapped() const final override {
-  	return t;
-  }
-  void render() const final override {
-	render_func(*t);
-  }
+	std::shared_ptr<T> t;
+	std::function<void(const T&)> render_func;
+
+	renderable_model(std::function<void(const T &)> render_func_, std::shared_ptr<T> t_):
+		render_func{render_func_},
+		t{t_}
+	{}
+	std::shared_ptr<const void> ptr_to_wrapped() const final override {
+		return t;
+	}
+	void render() const final override {
+		render_func(*t);
+	}
 };
 
-struct generic_node {
-  void render() {
-    if (!pImpl) return;
-    pImpl->render();
-  }
-  std::shared_ptr<const void> ptr_to_wrapped() {
-  	return pImpl->ptr_to_wrapped();
-  }
-  template<class T,class...Us>
-  generic_node(std::function<void(const T&)> render_func_, std::shared_ptr<T> t_) :
-    pImpl(std::make_shared<renderable_model<T>>(render_func_, t_)) {}
+struct node {
+	void render() {
+		if (p_renderable) {p_renderable->render();}
+	}
+	std::shared_ptr<const void> ptr_to_wrapped() {
+		return p_renderable->ptr_to_wrapped();
+	}
+
+	template<class WRAPPED,class RENDERER = DEFAULT_RENDERER>
+	node(std::shared_ptr<WRAPPED> w, RENDERER renderer = RENDERER()) :
+	p_renderable(std::make_shared<renderable_model<WRAPPED>>(
+		std::function<void(const WRAPPED&)>(static_cast<void(*)(const WRAPPED&)>(RENDERER::render)),
+		w)
+	)
+	{}
+
+	template<class WRAPPED, class RENDERER = DEFAULT_RENDERER>
+	node(const WRAPPED &w, RENDERER renderer = RENDERER()) :
+		node(std::make_shared<WRAPPED>(w), renderer) {}
+
+	node() : p_renderable() {}
 
 private:
-  std::shared_ptr<renderable_concept> pImpl;
+	std::shared_ptr<renderable_concept> p_renderable;
 };
 
 struct rect_node {
@@ -117,19 +122,14 @@ void caps_renderer::render<text_node>(const text_node &tn) {
 	std::cout << upper_text << std::endl;
 }
 
-template<class NODE,class RENDERER = DEFAULT_RENDERER>
-generic_node make_node(std::shared_ptr<NODE> n, RENDERER renderer = RENDERER()) {
-	std::function<void(const NODE&)> rndr = static_cast<void(*)(const NODE&)>(RENDERER::render);
-	return generic_node{rndr,n};
-}
-
 int main() {
 	auto tn = std::make_shared<text_node>("hello");
-	std::vector<generic_node> nodes{};
-	nodes.push_back(make_node(tn));
-	nodes.push_back(make_node(tn,normal_renderer{}));
-	nodes.push_back(make_node<text_node,normal_renderer>(tn));
-	for (generic_node n : nodes) {n.render();}
+	std::vector<node> nodes{};
+	nodes.push_back(node(tn));
+	nodes.push_back(node(tn,normal_renderer{}));
+	nodes.push_back(node(tn,caps_renderer{}));
+	nodes.push_back(node(text_node{"again"},caps_renderer{}));
+	for (node n : nodes) {n.render();}
 	tn->text = "world";
-	for (generic_node n : nodes) {n.render();}
+	for (node n : nodes) {n.render();}
 }
