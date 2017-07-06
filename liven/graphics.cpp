7169beb846cb358b2c1bc59f7aed33d432b0e5fc
Chad Mills <chad.mills@gmail.com>
2017-07-06 00:18:49 -0400

    remove most liven files.  leave basic design files only.

--- liven/graphics.cpp
+++
@@ -1,7 +0,0 @@
-#include "graphics.h"
-
-std::ostream &operator<<(std::ostream &os, const point<2> &p) {os << "(" << p.x << "," << p.y << ")"; return os;}
-std::ostream &operator<<(std::ostream &os, const point<3> &p) {os << "(" << p.x << "," << p.y << "," << p.z << ")"; return os;}
-
-point<3>::point(const point<2> &other) : x(other.x), y(other.y), z(0) {}
-point<2>::point(const point<3> &other) : x(other.x), y(other.y) {}
