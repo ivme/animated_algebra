52d7a51799a519531ecc86dd4806bd4ce4e7fc05
Chad Mills <chad.mills@gmail.com>
2017-07-12 11:19:39 -0400

    fix output operator bug

--- liven/graphics.cpp
+++ liven/graphics.cpp
@@ -1,9 +1,11 @@
 #include "graphics.h"
 
-using namespace liven;
+namespace liven {
 
 std::ostream &operator<<(std::ostream &os, const point<2> &p) {os << "(" << p.x << "," << p.y << ")"; return os;}
 std::ostream &operator<<(std::ostream &os, const point<3> &p) {os << "(" << p.x << "," << p.y << "," << p.z << ")"; return os;}
 
 point<3>::point(const point<2> &other) : x(other.x), y(other.y), z(0) {}
 point<2>::point(const point<3> &other) : x(other.x), y(other.y) {}
+
+}
