4e43ac471dd3671bd3989f47815943c0aeda034f
Chad Mills <chad.mills@gmail.com>
2017-07-14 01:52:04 -0400

    test viewer

--- liven/tests/test_viewer.cpp
+++ liven/tests/test_viewer.cpp
@@ -1,1 +1,19 @@
 #include "viewer.h"
+#include "animation.h"
+#include "image.h"
+#include <cassert>
+
+using namespace liven;
+
+int main() {
+	unsigned int n = 58;
+	unsigned int m = 100;
+	matrix<wchar_t> m1{n,m, L'e'};
+	ascii_image img1(m1);
+	matrix<wchar_t> m2{n,m, L'f'};
+	ascii_image img2(m2);
+	ascii_viewer vwr{};
+	animation<ascii_image> a{{m1,m2}};
+	a.set_animation_speed(2);
+	vwr.present(a);
+}
