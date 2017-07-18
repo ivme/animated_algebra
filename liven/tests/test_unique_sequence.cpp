4561ba47a5d7e5d434ae41ca8fec4ccf3575934b
Chad Mills <chad.mills@gmail.com>
2017-07-18 18:58:53 -0400

    test splice()

--- liven/tests/test_unique_sequence.cpp
+++ liven/tests/test_unique_sequence.cpp
@@ -4,7 +4,7 @@
 using namespace liven;
 
 int main() {
-	unique_sequence<int> s1;
+	unique_sequence<int> s1{};
 	s1.push_back(4);
 	assert(s1.size() == 1);
 	s1.push_back(4);
@@ -14,10 +14,16 @@ int main() {
 	assert(*s1.begin() == 3);
 	assert(*(++s1.begin()) == 4);
 
-	unique_sequence<int> s2;
+	unique_sequence<int> s2{};
 	s2.push_back(11);
 	s2.push_back(12);
 	s1.splice(s1.end(),s2);
 	assert(s1.size() == 4);
 	assert(s2.size() == 0);
+
+	unique_sequence<int> s3{};
+	s3.push_back(13);
+	s3.splice(s3.begin(),s1);
+	assert(*s3.begin() == 3);
+	assert(s3.size() == 5);
 }
